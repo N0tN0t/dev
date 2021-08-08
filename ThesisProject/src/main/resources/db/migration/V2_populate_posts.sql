589a42b35363c86b8d9b2d037c68743422be0b0f
N0tN0t <ssundertaless@gmail.com>
2021-08-08 19:49:40 +0300

    Commit_4.1

--- ThesisProject/src/main/resources/db/migration/V2_populate_posts.sql
+++ ThesisProject/src/main/resources/db/migration/V2_populate_posts.sql
@@ -1,1 +1,1 @@
-insert into posts (id,is_active,moderation_id,user_id,time,title,text,view_count) VALUES (1,1,"NEW",null,1,0,"Как создать проект","Смотрите во второй части ->",0)
+insert into posts (id,is_active,moderation_status,moderation_id,user_id,time,title,text,view_count) VALUES (1,1,"NEW",null,1,0,"Как создать проект","Смотрите во второй части ->",0)
