5aa8795c37e7254aa83c6e7ae739760313090045
N0tN0t <ssundertaless@gmail.com>
2021-08-15 18:37:00 +0300

    Commit_4

--- ThesisProject/src/main/resources/db/migration/V1_0__create_posts.sql
+++ ThesisProject/src/main/resources/db/migration/V1_0__create_posts.sql
@@ -2,7 +2,7 @@ drop table if exists posts;
 create table posts (
    id int4 NOT NULL,
    is_active boolean NOT NULL,
-   moderation_status varchar(255) NOT NULL,
+   moderation_status varchar(255),
    moderation_id integer NOT NULL,
    user_id int4 NOT NULL,
    time timestamp NOT NULL,
