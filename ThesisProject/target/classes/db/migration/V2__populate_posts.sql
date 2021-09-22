fd3263df2ff4c369b76992be58009a919b022edd
N0tN0t <87308878+N0tN0t@users.noreply.github.com>
2021-09-22 16:11:39 +0300

    Delete ThesisProject/target/classes directory

--- ThesisProject/target/classes/db/migration/V2__populate_posts.sql
+++
@@ -1,1 +0,0 @@
-insert into posts (id,is_active,moderation_status,moderation_id,user_id,time,title,text,view_count) VALUES (1,1,1,null,1,CURRENT_DATE,'Как создать проект','Смотрите во второй части ->',0)
