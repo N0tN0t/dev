f97635cc60f20db585570e81a81732b481aacd04
N0tN0t <ssundertaless@gmail.com>
2022-06-20 12:19:39 +0300

    Commit_23

--- src/main/java/main/respositories/PostRepository.java
+++ src/main/java/main/respositories/PostRepository.java
@@ -4,6 +4,7 @@ import main.entities.Posts;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +12,24 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts,Integer> {
+    @Query(value = "SELECT * FROM posts WHERE is_active = 1 " +
+            "AND moderation_status = 'ACCEPTED' AND posts.time <= NOW() " +
+            "ORDER BY posts.time DESC",
+            nativeQuery = true)
     public Page<Posts> findRecentPosts(Pageable pageable);
+    @Query(value = "SELECT * FROM posts WHERE is_active = 1 " +
+            "AND moderation_status = 'ACCEPTED' AND posts.time <= NOW() " +
+            "ORDER BY (SELECT COUNT(*) FROM post_votes WHERE post_id = posts.id) DESC",
+            nativeQuery = true)
     public Page<Posts> findBestPosts(Pageable pageable);
+    @Query(value = "SELECT * FROM posts WHERE is_active = 1 " +
+            "AND moderation_status = 'ACCEPTED' AND posts.time <= NOW() " +
+            "ORDER BY posts.time ASC",
+            nativeQuery = true)
     public Page<Posts> findEarlyPosts(Pageable pageable);
+    @Query(value = "SELECT * FROM posts WHERE is_active = 1 " +
+            "AND moderation_status = 'ACCEPTED' AND posts.time <= NOW() " +
+            "ORDER BY (SELECT COUNT(*) FROM post_comments WHERE post_id = posts.id) DESC",
+            nativeQuery = true)
     public Page<Posts> findPopularPosts(Pageable pageable);
 }
