42f7e1105499b098f3610bea927b922c97ded537
N0tN0t <ssundertaless@gmail.com>
2022-08-05 15:00:24 +0300

    Commit_45

--- src/main/resources/db/migration/V2__populate_posts.sql
+++ src/main/resources/db/migration/V2__populate_posts.sql
@@ -1,1 +1,7 @@
-insert into posts (id,is_active,moderation_status,moderation_id,user_id,time,title,text,view_count) VALUES (1,1,1,null,1,CURRENT_DATE,'Как создать проект','Смотрите во второй части ->',0)
+insert into posts (id,is_active,moderation_status,moderation_id,user_id,time,title,text,view_count) VALUES (1,1,1,null,1,CURRENT_DATE,'Как создать проект','Смотрите во второй части ->',0);
+INSERT INTO global_settings(code, name, value)
+VALUES ('MULTIUSER_MODE', 'Многопользовательский режим', 'YES');
+INSERT INTO global_settings(code, name, value)
+VALUES ('POST_PREMODERATION', 'Премодерация постов', 'YES');
+INSERT INTO global_settings(code, name, value)
+VALUES ('STATISTICS_IS_PUBLIC', 'Показывать всем статистику блога ', 'YES');
